3391d48fad382f5423e7cb6ffc7307ca00f6c0c2
Aubrian Halili <aubrian.halili@qred.com>
2023-05-10 11:14:24 +0200

    Update the dir path

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   devtool: 'inline-source-map',
   target: 'node',
   entry: [
-    path.join(__dirname, 'src/lambdas/external/transaction/index.ts'),
-    path.join(__dirname, 'src/lambdas/external/validations/index.ts'),
+    path.join(__dirname, '../src/lambdas/external/transaction/index.ts'),
+    path.join(__dirname, '../src/lambdas/external/validations/index.ts'),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
@@ -15,7 +15,7 @@ module.exports = {
   output: {
     libraryTarget: 'commonjs2',
     filename: 'index.js',
-    path: path.resolve(__dirname, '.localenv/dist'),
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
